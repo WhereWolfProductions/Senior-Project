44b5f141a1d170c96eed110481f4a9ba973b922c
WhereWolfProductions <35239712+WhereWolfProductions@users.noreply.github.com>
2018-01-22 09:11:41 -0800

    Possible setRotation

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -23,17 +23,15 @@ public class playerController : MonoBehaviour {
 
 
 
-        //transform.rotation = Quaternion.Slerp(transform.rotation, cam.rotation, Time.deltaTime * 100000);
-
-        //euler angles gives best result, but if you only set y, it fails to work
-
-        Vector3 eulerRot = new Vector3(cam.localEulerAngles.x, cam.eulerAngles.y, cam.eulerAngles.z);
-
-        transform.rotation = Quaternion.Euler(eulerRot);
 
     }
 
-
+	
+        void setRotation()
+	{
+		//Possible way to make two objects have same y rotation.
+		transfrom.localEulerAngles = new Vectro3(transform.localEulerAngles.x, transform.localEulerAngles.y  - ( (transform.localEulerAngles.y - transform.localEulerAngles.y) + cam.localEulerAngles.y), transform.lcoalEulerAngles.z)
+	}
 
     private void FixedUpdate()
     {
