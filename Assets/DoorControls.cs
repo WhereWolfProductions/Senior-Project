4fa25ef20546c5445099b3bd5b8263056aa19908
WhereWolfProductions <35239712+WhereWolfProductions@users.noreply.github.com>
2018-01-23 09:19:38 -0800

    Beginning of door controls

--- Assets/DoorControls.cs
+++ Assets/DoorControls.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorControls : MonoBehaviour {
-
+	
+	public bool locked;  //Used to set some doors to be unopenable, this is used with doors to nowhere.
+	bool open;
+	
+	Animator doorAnimController;
+	
+	Vector3 playerPos;
+	
 	// Use this for initialization
 	void Start () {
+		
+		//Assures that if I forget to set door state, it will be defaultly unopenable.
+		if(locked == null)
+		{
+			locked = false;
+		}
+		
+		doorAnimController = gameObject.GetComponent<Animator>();
+		playerPos = GameObject.FindWithTag("Player").transform.position;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
+	
+	
+	void clickedButton()
+	{
+		//If unlocked and closed...
+		if(locked == false && open == false)
+		{
+			doorAnimController.SetBool("opened", true);
+		}
+	}
+	
+	
+	void autoClose()
+	{
+		if(Vector3.Distance(transform.position, playerPos) > 4)
+		{
+			doorAnimController.SetBool("opened", false);
+		}
 		
 	}
 }
