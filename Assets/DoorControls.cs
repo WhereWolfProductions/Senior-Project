34ad7b5c1eee70bb990a3e82af5378139ea77c21
WhereWolfProductions <35239712+WhereWolfProductions@users.noreply.github.com>
2018-01-23 11:44:37 -0800

    Update DoorControls.cs

--- Assets/DoorControls.cs
+++ Assets/DoorControls.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorControls : MonoBehaviour {
-	
-	public bool locked;  //Used to set some doors to be unopenable, this is used with doors to nowhere.
+		
+	bool locked;  //The state the door is in
 	bool open;
 	
 	Animator doorAnimController;
@@ -28,25 +28,41 @@ public class DoorControls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+		
+		
 	}
 	
 	
 	void clickedButton()
 	{
-		//If unlocked and closed...
+		
+		//If unlocked(green) and closed...
 		if(locked == false && open == false)
 		{
 			doorAnimController.SetBool("opened", true);
 		}
+		//unlocked(green) and open
+		else if(locked == false && open == true)
+		{
+			doorAnimController.SetBool("opened", false)
+		}
+		//locked(red) in any state
+		else if (locked == true)
+		{
+			//play locked anim.
+		}
+		
+		//run function with while loop to make button uninteractalbe until anim is done playing.
+		
 	}
 	
 	
 	void autoClose()
 	{
+		//If distance between door and player gets big enough, the door will set itself to clsoed, thus closing.
 		if(Vector3.Distance(transform.position, playerPos) > 4)
 		{
 			doorAnimController.SetBool("opened", false);
 		}
-		
 	}
 }
